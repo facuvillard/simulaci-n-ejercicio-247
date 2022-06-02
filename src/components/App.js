88603d49d3824ec9d881fbe59060f46422b9802d
fvillard <facundo.villard@mercadolibre.com>
2022-06-02 12:04:37 -0300

    recharts

--- src/components/App.js
+++ src/components/App.js
@@ -12,15 +12,7 @@ export default function App() {
         <Grid xs={12}>
         </Grid>
         <Grid>
-          <p>
-            Una central telefónica cuenta con 8 líneas hacia el exterior. Varios internos intentan tomar la línea con una
-            frecuencia de 1 cada 90 +/- 30 seg. Las llamadas tienen una duración de 5 +/- 1 minuto. <br/>
-            Los internos que no logran tomar la línea vuelven a intentar luego de un lapso de 2 +/- 1 minuto.
-            Simular durante 8 horas, calculando:<br/>
-            Promedio de Tiempo de cada llamada (a partir de que logra comunicarse).<br/>
-            Promedio de Tiempo Total (desde el primer intento hasta que deja de hablar).<br/>
-            Promedio de Cantidad de intentos de toma de línea de cada llamada.<br/>
-          </p>
+          <p>Enunciado</p>
         </Grid>
       </Grid>
     </div>
